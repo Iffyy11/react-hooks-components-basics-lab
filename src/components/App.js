5ddcf53aee4bd1778836a74a3edea6b12ab7ff55
ifra abdi issack <ifraha461@gmail.com>
2025-07-18 22:09:47 +0300

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,38 @@
 import React from "react";
-import Navbar from "../Navbar";
-import Home from "../Home";
-import About from "../About"; // ✅ Import About
+
+function NavBar() {
+  return (
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+{/* write an <About> component here */}
+
+function About(){
+  return(
+    <div id = "about">
+      <h1>About</h1>
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <NavBar />
       <Home />
-      <About /> {/* ✅ Render About */}
+      <About />
     </div>
   );
 }
